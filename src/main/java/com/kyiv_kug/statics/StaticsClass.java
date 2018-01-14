23d6d7721ab23c112246501b2dfade2c9d343675
Sergiy Gnatyuk <surpsg2012@gmail.com>
2018-01-14 13:41:48 +0200

    added statics example

---
+++ src/main/java/com/kyiv_kug/statics/StaticsClass.java
@@ -0,0 +1,14 @@
+package com.kyiv_kug.statics;
+
+public class StaticsClass {
+
+    public static final int CONST = 1;
+    public static final Object CONST_OBJ = new Object();
+
+    public static int staticInt = 2;
+    public static Object staticObj = new Object();
+
+    public static int staticMethod(){
+        return 1;
+    }
+}

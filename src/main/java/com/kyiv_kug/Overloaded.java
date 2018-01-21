70ea3dd660c4e9c3ebb58c85d46c131842db96b4
Sergiy Gnatyuk <surpsg2012@gmail.com>
2018-01-21 19:36:23 +0200

    overloaded methods example

---
+++ src/main/java/com/kyiv_kug/Overloaded.java
@@ -0,0 +1,21 @@
+package com.kyiv_kug;
+
+public class Overloaded {
+    void doSomething(){
+        doSomething(0);
+    }
+
+    void doSomething(int a){
+        doSomething(a, "");
+    }
+
+    void doSomething(int a, String str){
+        doSomething(a, str, false);
+    }
+
+    void doSomething(int a, String str, boolean bool){
+    }
+}
+
+
+

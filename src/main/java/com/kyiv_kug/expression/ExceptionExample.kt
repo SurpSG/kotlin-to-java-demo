a921ae8d3f01221e6b8cef1488a63c7e6c0691d8
Ievgenii Lopushen <ievgenii@comergence.biz>
2018-01-21 21:21:13 +0200

    exceptions

--- src/main/java/com/kyiv_kug/expression/ExceptionExample.kt
+++ src/main/java/com/kyiv_kug/expression/ExceptionExample.kt
@@ -2,13 +2,12 @@ package com.kyiv_kug.expression
 
 fun main(args: Array<String>) {
     val list = listOf(1,2,3,4)
-    val index = 0
+    val index = 6
     val msg = try {
-        "$index element is: ${list[0]}"
+        "$index element is: ${list[index]}"
     } catch (e: Exception) {
-        e.message ?: e::class.java.name
-    } finally {
-        "no content"
-    }
+       "${ e.message}"
+    }.trim()
+
     println(msg)
 }

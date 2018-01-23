2423c61850049283e31c0e8902202dbdd3a76e1d
Sergiy Gnatyuk <surpsg2012@gmail.com>
2018-01-24 01:31:32 +0200

    refactoring

--- src/main/java/com/kyiv_kug/statics/declaration/StaticsClassKotlin.java
+++ src/main/java/com/kyiv_kug/statics/declaration/StaticsClassKotlin.java
@@ -5,6 +5,8 @@ public class StaticsClassKotlin {
     public static final int CONST = 1;
     public static final Object CONST_OBJ = new Object();
 
+    void simpleFun(){}
+
     public static int staticMethod(){
         return 1;
     }
